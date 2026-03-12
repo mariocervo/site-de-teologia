dbc6340f69c7a39c62ea69ec30a8dafeef59def4
mariocervo <mariocervo@outlook.com>
2026-03-11 22:59:14 -0300

    Update data.js

--- data.js
+++ data.js
@@ -1,48 +1,41 @@
 export const categories = [
-{ id: 'todos', name: 'Todos' },
-{ id: 'teologia', name: 'Teologia' },
-{ id: 'escatologia', name: 'Escatologia' },
-{ id: 'hermeneutica', name: 'Hermenêutica' },
-{ id: 'estudo-biblico', name: 'Estudo Bíblico' }
+  { id: 'todos', name: 'Todos' },
+  { id: 'teologia', name: 'Teologia' },
+  { id: 'escatologia', name: 'Escatologia' },
+  { id: 'hermeneutica', name: 'Hermenêutica' },
+  { id: 'estudo-biblico', name: 'Estudo Bíblico' }
 ]
-
 
 export const ebooks = [
-
-{
-id:1,
-title:'67 Ideias de Pregações',
-description:'Tem dificuldade em transformar seus conhecimentos bíblicos em pregações impactantes? Com este e-book você terá acesso a esboços prontos.',
-price:'De R$ 97,00 por 6x de R$ 08,82',
-category:'estudo-biblico',
-image:'img/pregacao.webp',
-hotmartLink:'https://pay.hotmart.com/A104759967E?checkoutMode=10&split=6'
-},
-
-{
-id:2,
-title:'Apocalipse Descomplicado',
-description:'Descubra os significados do Apocalipse e compreenda suas visões simbólicas.',
-price:'De R$ 97,00 por 6x de R$ 08,82',
-category:'escatologia',
-image:'img/apocalipse.webp',
-hotmartLink:'https://pay.hotmart.com/W104760699Q?checkoutMode=10&split=6'
-},
-
-{
-id:3,
-title:'Bíblia na Ponta da Língua',
-description:'Aprenda técnicas de memorização bíblica usando repetição estratégica.',
-price:'De R$ 97,00 por 6x de R$ 08,82',
-category:'estudo-biblico',
-image:'img/biblia.webp',
-hotmartLink:'https://pay.hotmart.com/R104774512W?checkoutMode=10'
-}
-
+  {
+    id:1,
+    title:'67 Ideias de Pregações',
+    description:'Tem dificuldade em transformar seus conhecimentos bíblicos em pregações impactantes? Com este e-book você terá acesso a esboços prontos.',
+    price:'De R$ 97,00 por 6x de R$ 08,82',
+    category:'estudo-biblico',
+    image:'img/pregacao.webp',
+    hotmartLink:'https://pay.hotmart.com/A104759967E?checkoutMode=10&split=6'
+  },
+  {
+    id:2,
+    title:'Apocalipse Descomplicado',
+    description:'Descubra os significados do Apocalipse e compreenda suas visões simbólicas.',
+    price:'De R$ 97,00 por 6x de R$ 08,82',
+    category:'escatologia',
+    image:'img/apocalipse.webp',
+    hotmartLink:'https://pay.hotmart.com/W104760699Q?checkoutMode=10&split=6'
+  },
+  {
+    id:3,
+    title:'Bíblia na Ponta da Língua',
+    description:'Aprenda técnicas de memorização bíblica usando repetição estratégica.',
+    price:'De R$ 97,00 por 6x de R$ 08,82',
+    category:'estudo-biblico',
+    image:'img/biblia.webp',
+    hotmartLink:'https://pay.hotmart.com/R104774512W?checkoutMode=10'
+  }
 ]
-
 
 export const courses = []
 export const articles = []
 export const testimonials = []
-
